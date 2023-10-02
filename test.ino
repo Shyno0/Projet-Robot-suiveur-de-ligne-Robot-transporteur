b2b851d28c3f04a23d5a0d35028998c6cc267214
Thib <54439541+ThibLeVrai@users.noreply.github.com>
2023-10-02 10:04:21 +0200

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,82 @@
+unsigned int cg,cm,cd;
+unsigned long cgadd = 0,cmadd = 0,cdadd = 0;
+const char LEDg = 4, LEDm = 7, LEDd = 8;
+char i;
+bool poids;
+
+void setup()
+{
+  //Tests
+  Serial.begin(9600);
+  // Declare tous les PINs des moteurs en sortie
+  pinMode(6, OUTPUT); // Déclare Moteur Gauche
+  pinMode(5, OUTPUT); // Déclare Moteur Droit
+  //pinMode(ERROR_LED, OUTPUT);
+
+  // Declare tous les PINs des capteurs en entrée
+  pinMode(A0, INPUT); // Déclare Capteur Gauche
+  pinMode(A1, INPUT); // Déclare Capteur Milieu
+  pinMode(A2, INPUT); // Déclare Capteur Droit
+  pinMode(3, INPUT); // Déclare Capteur de Masse
+
+  // Declare tous les PINs des LEDs en sortie
+  pinMode(LEDg, OUTPUT);
+  pinMode(LEDm, OUTPUT);
+  pinMode(LEDd, OUTPUT);
+}
+
+// Programme principal [boucle]
+void loop()
+{
+  poids = digitalRead(3); // Lecture de la broche 3 (Capteur de masse)
+
+  cgadd = 0;
+  cmadd = 0;
+  cdadd = 0;
+
+  for(i=20; i<0; i--)
+  {
+    cg = analogRead(A0);
+    cgadd = cgadd+cg;
+    Serial.print(cgadd);
+    delay(1);
+  }
+  cgadd = cgadd/20;
+
+  for(i=20; i<0; i--)
+  {
+    cm = analogRead(A1);
+    cmadd = cmadd + cm;
+    delay(1);
+  }
+  cmadd = cmadd/20;
+
+  for(i=20; i<0; i--)
+  {
+    cd = analogRead(A2);
+    cdadd = cdadd+cd;
+    delay(1);
+  }
+  cdadd = cdadd/20;
+  
+  if(cgadd<=25){
+    digitalWrite(LEDg, HIGH);} 
+  else{digitalWrite(LEDg, LOW);}
+  if(cmadd<=25){
+    digitalWrite(LEDm, HIGH);} 
+  else{digitalWrite(LEDm, LOW);}
+  if(cdadd<=25){
+    digitalWrite(LEDd, HIGH);} 
+  else{digitalWrite(LEDd, LOW);}
+
+  Serial.print("Capteur masse  : ");  
+  Serial.print(poids);
+  Serial.print("\nCapteur gauche  : ");  
+  Serial.print(cgadd);
+  Serial.print("\nCapteur milieu  : ");  
+  Serial.print(cmadd);
+  Serial.print("\nCapteur droit  : ");  
+  Serial.print(cdadd);
+  Serial.print("\n\n");
+  delay(600);
+}
